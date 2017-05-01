22b68969daa44e7003e7a02f7a155067df2554e9
Adam Martinez <zach1392@yahoo.com>
2017-05-01 18:29:09 -0500

    Add files via upload

---
+++ Scoring_Names.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+class names {
+public:
+	void lineArray(fstream &infile) {	//make array based
+		int n;							//on longest name
+		while (getline(infile, line)) {
+			int biggest_name = 0;
+			
+			if (line.length > biggest_name)
+				biggest_name = line.length;
+			n = biggest_name;
+		}
+		
+		int *arr_p;					//dynamically allocate arr length
+		arr_p = new int[n];
+
+	}
+
+	void nameCount(fstream &infile) {
+		char arr1[10];
+
+		while(getline (infile, line)) {
+			
+			
+
+		}
+
+	}
+
+	int alphaVal() {
+		
+		char alphaChar = 'a';
+
+		for (int i; i < 27; i++) {
+			alphaChar = i;
+			alphaChar++;
+		}
+	}
+	
+
+private:
+	string line;
+
+};
+
+int main()
+{
+	
+
+	names alphaVal;
+
+	fstream infile;
+
+	infile.open("names.txt", ios::out | ios::in);
+
+	names nameCount;
+
+
+	infile.close();
+
+}
+
+
+
