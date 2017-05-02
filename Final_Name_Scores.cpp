b63d0fc342fb4a4e047f8cdf71be5dcd5e9408cb
Adam Martinez <zach1392@yahoo.com>
2017-05-02 04:52:49 -0500

    Update Final_Name_Scores.cpp

--- Final_Name_Scores.cpp
+++ Final_Name_Scores.cpp
@@ -2,26 +2,53 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
 
+void alphabetList(fstream &infile, fstream &outfile) {
+	string nameList[6000];
+	string line;
+	int i = 0;
+	while (!infile.eof()) {				//make array of names
+		getline(infile, line);
+		nameList[i] = line;
+		i++;
+		}
+	
+	sort(begin(nameList), end(nameList));		//sort names in alphabetical in array
+	int j = 0;
+	for (j; j < sizeof(nameList) -1 ; j++) {
+		if(!nameList[j].empty()) {
+			outfile << nameList[j] << endl;	//rewrite alphabetical list to file
+		}
+	}
+}
 
-	void nameCount(fstream &infile) {			//ready function for passing file by reference
-		char arr1[12];					//initialize array big enough to fit biggest name
-		int nameSum = 0;
+
+	void nameCount(fstream &infile) {		//ready function for passing file by reference
+		char arr1[12];				//initialize array big enough to fit biggest name
 		string line;
-		while(!infile.eof()) {				//read until end of file
-			getline(infile, line);			//store each name in line variable
+		int final_Score = 0;
+		int listPosition = 1;
+		while(!infile.eof()) {			//read until end of file
+			getline(infile, line);		//store each name in line variable
 			stringstream ss(line);
-			ss >> arr1;				//store name into array
+			ss >> arr1;			//store name into array
+			int listPosition = 1;		//initialize position of name variable
+			int nameSum = 0;
+			int nameProduct = 0;
+
 			int i = 0;
 			for (i; i < strlen(arr1) -1; i++) {
 
-				nameSum = arr1[i] + nameSum;	//sum of each line
-								//of letters added
+				nameSum = arr1[i] + nameSum;				//sum of each line
+				nameProduct = nameSum * 	listPosition;		//sum * position of name in list	
 			}
+			final_Score = nameProduct + final_Score;
+			listPosition++;							//iterating position after every line
 		}
-		cout << nameSum << endl;
+		cout << final_Score << endl;						//print name score
 	}
 
 	void alphaVal() {				//give each letter of 
@@ -33,17 +60,20 @@ using namespace std;
 		}
 	}
 	
-	int main()					//execute main file
+	int main()						//execute main file
 	{
-		fstream infile;				//ready file for write/read
+		fstream infile,outfile;				//ready file for write/read
 
-		infile.open("names.txt");		//open file("names.txt") 
-		alphaVal();				//call function to give letters value
-		nameCount(infile);			//call function to sum file
+		infile.open("names.txt");			//open file("names.txt") 
+		outfile.open("names.txt");
+		alphaVal();					//call function to give letters value
+		alphabetList(infile, outfile);
+		nameCount(outfile);				//call function to sum file
 
-		infile.close();				//close file
-
-		getchar();				//keep debug window open
+		infile.close();					//close file
+		outfile.close();
+		getchar();					//keep debug window open
 	}
+
 
 
