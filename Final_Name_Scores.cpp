eb74bf18d27f2e808afe1458829ea40a35eb9357
Adam Martinez <zach1392@yahoo.com>
2017-05-02 02:58:38 -0500

    Update Final_Name_Scores.cpp

--- Final_Name_Scores.cpp
+++ Final_Name_Scores.cpp
@@ -6,26 +6,26 @@
 using namespace std;
 
 
-	void nameCount(fstream &infile) {
-		char arr1[12];
+	void nameCount(fstream &infile) {			//ready function for passing file by reference
+		char arr1[12];					//initialize array big enough to fit biggest name
 		int nameSum = 0;
 		string line;
-		while(!infile.eof()) {
-			getline(infile, line);
+		while(!infile.eof()) {				//read until end of file
+			getline(infile, line);			//store each name in line variable
 			stringstream ss(line);
-			ss >> arr1;
+			ss >> arr1;				//store name into array
 			int i = 0;
 			for (i; i < strlen(arr1) -1; i++) {
 
-				nameSum = arr1[i] + nameSum;
-
+				nameSum = arr1[i] + nameSum;	//sum of each line
+								//of letters added
 			}
 		}
 		cout << nameSum << endl;
 	}
 
 	void alphaVal() {				//give each letter of 
-								//alphabet a value
+							//alphabet a value
 		char alphaChar[25];
 		int i = 0;
 		for (i; i < 26; i++) {
@@ -33,16 +33,17 @@ using namespace std;
 		}
 	}
 	
-	int main()
+	int main()					//execute main file
 	{
-		fstream infile;
+		fstream infile;				//ready file for write/read
 
-		infile.open("names.txt");
-		alphaVal();
-		nameCount(infile);
+		infile.open("names.txt");		//open file("names.txt") 
+		alphaVal();				//call function to give letters value
+		nameCount(infile);			//call function to sum file
 
-		infile.close();
+		infile.close();				//close file
+
+		getchar();				//keep debug window open
 	}
-
 
 
