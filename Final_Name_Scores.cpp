f4b30f95729679611501cf2f02b1058521527fc6
Adam Martinez <zach1392@yahoo.com>
2017-05-02 13:48:39 -0500

    Update Final_Name_Scores.cpp

--- Final_Name_Scores.cpp
+++ Final_Name_Scores.cpp
@@ -6,74 +6,39 @@
 
 using namespace std;
 
-void alphabetList(fstream &infile, fstream &outfile) {
+void alphabetList(ifstream &infile, ofstream &outfile) {
 	string nameList[6000];
 	string line;
 	int i = 0;
+	infile.open("names.txt");
 	while (!infile.eof()) {				//make array of names
 		getline(infile, line);
 		nameList[i] = line;
 		i++;
 		}
+
 	
-	sort(begin(nameList), end(nameList));		//sort names in alphabetical in array
-	int j = 0;
-	for (j; j < sizeof(nameList) -1 ; j++) {
-		if(!nameList[j].empty()) {
-			outfile << nameList[j] << endl;	//rewrite alphabetical list to file
+	sort(begin(nameList), end(nameList));	//sort names in alphabetical in array
+	for (int k = 0; k < 6000; k++) {
+
+		if (!nameList[k].empty()) {
+			cout << nameList[k] << endl;
 		}
+		k++;
 	}
 }
 
 
-	void nameCount(fstream &infile) {		//ready function for passing file by reference
-		char arr1[12];				//initialize array big enough to fit biggest name
-		string line;
-		int final_Score = 0;
-		int listPosition = 1;
-		while(!infile.eof()) {			//read until end of file
-			getline(infile, line);		//store each name in line variable
-			stringstream ss(line);
-			ss >> arr1;			//store name into array
-			int listPosition = 1;		//initialize position of name variable
-			int nameSum = 0;
-			int nameProduct = 0;
-
-			int i = 0;
-			for (i; i < strlen(arr1) -1; i++) {
-
-				nameSum = arr1[i] + nameSum;				//sum of each line
-				nameProduct = nameSum * 	listPosition;		//sum * position of name in list	
-			}
-			final_Score = nameProduct + final_Score;
-			listPosition++;							//iterating position after every line
-		}
-		cout << final_Score << endl;						//print name score
-	}
-
-	void alphaVal() {				//give each letter of 
-							//alphabet a value
-		char alphaChar[25];
-		int i = 0;
-		for (i; i < 26; i++) {
-			alphaChar['a' + i] = i+1;		
-		}
-	}
 	
 	int main()						//execute main file
 	{
-		fstream infile,outfile;				//ready file for write/read
+		ifstream infile;
+		ofstream outfile;				//ready file for write/read
 
-		infile.open("names.txt");			//open file("names.txt") 
-		outfile.open("names.txt");
-		alphaVal();					//call function to give letters value
-		alphabetList(infile, outfile);
-		nameCount(outfile);				//call function to sum file
-
-		infile.close();					//close file
-		outfile.close();
+		
+		
+		alphabetList(infile, outfile);			//call alphabetical function
+		
+		
 		getchar();					//keep debug window open
 	}
-
-
-
