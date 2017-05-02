74a417721afa75c21c844555d5160c89c2fcddd1
Adam Martinez <zach1392@yahoo.com>
2017-05-02 01:18:05 -0500

    Update Scoring_Names.cpp

--- Scoring_Names.cpp
+++ Scoring_Names.cpp
@@ -7,69 +7,75 @@ using namespace std;
 
 class names {
 public:
-	int lineArray(fstream &infile) {	//make array based
-		int n;							//on longest name
-		while (getline(infile, line)) {
-			int biggest_name = 0;
+	//int lineArray(fstream &infile) {	//make array based
+		//int n;							//on longest name
+		//while (getline(infile, line)) {
+			//int biggest_name = 0;
 			
-			if (line.length > biggest_name)
-				biggest_name = line.length;
-			n = biggest_name;
-		}
+			//if (line.length > biggest_name)
+				//biggest_name = line.length;
+			//n = biggest_name;
+		//}
 		
-		int *arr_p;					//dynamically allocate arr length
-		arr_p = new int[n];
+		//int *arr_p;					//dynamically allocate arr length
+		//arr_p = new int[n];
 
-	}
+	//}
 
 
 
 
 	void nameCount(fstream &infile) {
 		char arr1[10];
-
+		int nameSum = 0;
+		alphaVal();
 		while(getline (infile, line)) {
 			istringstream ss(line);
 
 			ss >> arr1;
+			int i = 0;
+			for (i; i < sizeof(arr1) -1; i++) {
 
+				nameSum = int(arr1[i]) + nameSum;
+				
+
+			}
 
 		
 
 		}
-
+		cout << nameSum << endl;;
 	}
 
-	int alphaVal() {				//give each letter of 
+	void alphaVal() {				//give each letter of 
 								//alphabet a value
-		char alphaChar = 'a';
+		char alphaChar[25];
 		int i = 0;
-
-		for (i; i < 27; i++) {
+		for (i; i < 26; i++) {
 			
-			alphaChar++;
+			alphaChar['a' + i] = i+1;
+			
 		}
 	}
 	
 
 private:
 	string line;
-	int nameScore;
 };
 
 int main()
 {
-	
-
-	names alphaVal;
 
 	fstream infile;
 
-	infile.open("names.txt", ios::out | ios::in);
+	infile.open("names.txt");
 
-	names nameCount;
-
-
+	names nameCount();
+	
 	infile.close();
 
+	cout << "Yes please" << endl;
+
+	getchar();
 }
+
